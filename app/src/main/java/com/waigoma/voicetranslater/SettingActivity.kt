1eda9446be0a9900ef6206e0c2f4efcf16a4e299
waigoma <waichi0302@gmail.com>
2023-01-07 22:47:03 +0900

    implement SearchSpinner

--- app/src/main/java/com/waigoma/voicetranslater/SettingActivity.kt
+++ app/src/main/java/com/waigoma/voicetranslater/SettingActivity.kt
@@ -3,18 +3,24 @@ package com.waigoma.voicetranslater
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.waigoma.voicetranslater.databinding.ActivitySettingBinding
+import com.waigoma.voicetranslater.setting.SettingData
+import com.waigoma.voicetranslater.setting.SettingOnClickListener
 
 class SettingActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySettingBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        SettingData.initialize()
 
         binding = ActivitySettingBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
+
+        setSpinner()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -36,5 +42,30 @@ class SettingActivity : AppCompatActivity() {
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun setSpinner() {
+        val textViewRec = findViewById<TextView>(R.id.sspinner_setting_rec)
+        val textViewTrans = findViewById<TextView>(R.id.sspinner_setting_trans)
+
+        textViewRec.setOnClickListener(
+            SettingOnClickListener(
+                this,
+                R.layout.dialog_searchable_spinner_rec,
+                R.id.dialog_edit_text_rec,
+                R.id.dialog_list_view_rec,
+                textViewRec
+            )
+        )
+
+        textViewTrans.setOnClickListener(
+            SettingOnClickListener(
+                this,
+                R.layout.dialog_searchable_spinner_trans,
+                R.id.dialog_edit_text_trans,
+                R.id.dialog_list_view_trans,
+                textViewTrans
+            )
+        )
     }
 }
